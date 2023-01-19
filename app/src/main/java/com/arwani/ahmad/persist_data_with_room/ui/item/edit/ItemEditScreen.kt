ae8b17618b497e185b8e19bd3b2fbcb05bad6b04
arwani <arwaniahmad659@gmail.com>
2023-01-19 18:09:54 +0700

    display edit screen

--- app/src/main/java/com/arwani/ahmad/persist_data_with_room/ui/item/edit/ItemEditScreen.kt
+++ app/src/main/java/com/arwani/ahmad/persist_data_with_room/ui/item/edit/ItemEditScreen.kt
@@ -1,8 +1,17 @@
 package com.arwani.ahmad.persist_data_with_room.ui.item.edit
 
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import com.arwani.ahmad.persist_data_with_room.InventoryTopAppBar
 import com.arwani.ahmad.persist_data_with_room.R
+import com.arwani.ahmad.persist_data_with_room.ui.AppViewModelProvider
+import com.arwani.ahmad.persist_data_with_room.ui.item.entry.ItemEntryBody
 import com.arwani.ahmad.persist_data_with_room.ui.navigation.NavigationDestination
+import kotlinx.coroutines.launch
 
 object ItemEditDestination : NavigationDestination {
     override val route: String
@@ -14,6 +23,30 @@ object ItemEditDestination : NavigationDestination {
 }
 
 @Composable
-fun ItemEditScreen(){
-
+fun ItemEditScreen(
+    modifier: Modifier = Modifier,
+    navigationBack: () -> Unit,
+    onNavigateUp: () -> Unit,
+    viewModel: ItemEditViewModel = androidx.lifecycle.viewmodel.compose.viewModel(factory = AppViewModelProvider.factory)
+) {
+    val coroutineScope = rememberCoroutineScope()
+    Scaffold(topBar = {
+        InventoryTopAppBar(
+            title = stringResource(id = ItemEditDestination.titleRes),
+            canNavigationBack = true,
+            navigateUp = onNavigateUp
+        )
+    }) { innerPadding ->
+        ItemEntryBody(
+            itemUiState = viewModel.itemUiState,
+            onItemValueChange = viewModel::updateUiState,
+            onSaveClick = {
+                coroutineScope.launch {
+                    viewModel.updateItem()
+                    navigationBack()
+                }
+            },
+            modifier = modifier.padding(innerPadding)
+        )
+    }
 }
